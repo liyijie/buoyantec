2cc4d186644d778edb4ce7468b74fafe8cc27e76
OnlyCZY <853852998@qq.com>
2015-06-11 22:25:01 +0800

    add link to home#index and reports

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @active_report = Report.order('created_at DESC').first
-    @reports = Report.order('created_at DESC').offset(1).first(6)
+    @reports = Report.last(3)
   end
 
   def error
