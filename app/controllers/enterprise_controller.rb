ec7eec75750f920df2a0725c6b13be4b20596b0c
EndingJason <www.zhangyi508@gmail.com>
2015-06-12 17:02:45 +0800

    change nav

--- app/controllers/enterprise_controller.rb
+++ app/controllers/enterprise_controller.rb
@@ -4,7 +4,7 @@ class EnterpriseController < ApplicationController
       {name: "大唐电信科技股份有限公司", logo: "logo_group.gif", url: "http://www.datang.com"},
       {name: "大唐移动通信设备有限公司", logo: "logo_dt.gif", url: "http://www.datangmobile.cn"},
       {name: "联芯科技有限公司", logo: "logo_lx.png", url: "http://www.leadcoretech.com"},
-      {name: "大唐高鸿数据网络技术股份有限公司", logo: "logo_gh.png", url: "http:/www.gohigh.com.cn"},
+      {name: "大唐高鸿数据网络技术股份有限公司", logo: "logo_gh.png", url: "http://www.gohigh.com.cn"},
       {name: "电信科学技术仪表研究所", logo: "logo_yb.png", url: "http://www.dxyb.com"},
       {name: "成都大唐线缆有限公司", logo: "logo_cdxl.png", url: "http://www.datang.com"},
       {name: "北京畅通达通信技术有限公司", logo: "logo_ctd.png", url: "http://www.ctdt.com.cn"},
