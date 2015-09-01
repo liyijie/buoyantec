19a710688ea4e47f736277f53766c4cf8b751a7c
OnlyCZY <853852998@qq.com>
2015-09-01 09:18:03 +0800

    @import bootstrap;

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,12 @@
 class HomeController < ApplicationController
   def index
-    num = Report.where(category:1).count
-    if num < 5
-    	@firms = Report.where(category:1).order("created_at DESC").limit(num)
-    	@trades = Report.where(category:2).order("created_at DESC").limit(5 - num)
+    company_news = Report.where(category:1).order("created_at DESC")
+    if company_news.count < 5
+      @firms = company_news.limit(company_news.count)
+      @trades = Report.where(category:2).order("created_at DESC").limit(5 - company_news.count)
     else
-    	@firms = Report.where(category:1).order("created_at DESC").limit(5)
-    	@trades = Report.where(category:2).order("created_at DESC").limit(0)
+      @firms = company_news.limit(5)
+      @trades = []
     end
   end
 
